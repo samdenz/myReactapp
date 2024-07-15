c2123f0429c73334f50dad1c7ca0050c32771f59
samdenz <denzelsam.omondi@strathmore.edu>
2024-07-15 11:23:11 +0300

    uploads

--- src/index.js
+++ src/index.js
@@ -1,15 +1,21 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
+import { createRoot } from 'react-dom/client';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { BrowserRouter } from 'react-router-dom';
+import reportWebVitals from './reportwebVitals';
+// Get the root element from the DOM
+const container = document.getElementById('root');
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+// Create a root
+const root = createRoot(container);
+
+// Initial render
 root.render(
-  <React.StrictMode>
+  <BrowserRouter>
     <App />
-  </React.StrictMode>
+  </BrowserRouter>
 );
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
